5985ce1e30553a5a4fa5e6d03e04de162df1a9b7
jrasero <jrasero.daparte@gmail.com>
2021-03-29 11:36:33 -0400

    polish code

--- cofluctuate_bold_glm/edge_label_masker.py
+++ cofluctuate_bold_glm/edge_label_masker.py
@@ -23,40 +23,39 @@ def compute_edge_ts(roi_mat):
         for jj in range(ii+1, n_rois):
             edge_mat[ii, jj, 0, :] = roi_mat[:,ii]*roi_mat[:,jj]
             edge_mat[jj, ii, 0,:] = edge_mat[ii, jj, 0, :]
-        
+
     return edge_mat
 
 
 class NiftiEdgeAtlas():
-    
-    
-    def __init__(self, 
+
+    def __init__(self,
                  atlas_file,
                  detrend = False,
                  low_pass = None,
-                 high_pass= None, 
+                 high_pass= None,
                  t_r = None,
                  fir_delays=[0]
                 ):
-        
+
         self.atlas_file = atlas_file
         self.detrend = detrend
         self.low_pass = low_pass
         self.high_pass = high_pass
         self.t_r = t_r
         self.fir_delays = fir_delays
-        
-    def fit_transform(self, 
-                      run_img, 
-                      events=None, 
-                      confounds=None):
-        
+
+    def fit_transform(self,
+                      run_img,
+                      events = None,
+                      confounds = None):
+
         run_img = load_img(run_img)
         n_scans = run_img.shape[3]
-        
+
         #TODO: See if it makes sense to create a function for this
         # or a base class that has this method
-        
+
         # 1- Load and compute FIR events
         task_conf = None
         if events is not None:
@@ -64,43 +63,41 @@ class NiftiEdgeAtlas():
                 assert os.path.exists(events)
                 assert events.endswith("events.tsv")
                 events_mat = pd.read_csv(events, sep="\t")
-            
+
                 start_time = 0
                 end_time = (n_scans - 1)* self.t_r
                 frame_times = np.linspace(start_time, end_time, n_scans)
                 task_conf = create_task_confounders(frame_times, events_mat, 
                                                     fir_delays=self.fir_delays)
-                
+
             elif isinstance(events, np.ndarray):
                 # You can supply a given task matrix to denoise
                 task_conf = events
-            
-            
+
         self.task_conf_ = task_conf
-        
+
         # 2- Parcellate data
-        label_masker = NiftiLabelsMasker(labels_img=self.atlas_file, 
-                                         detrend=self.detrend,
+        label_masker = NiftiLabelsMasker(labels_img = self.atlas_file,
+                                         detrend = self.detrend,
                                          low_pass = self.low_pass,
                                          high_pass = self.high_pass,
-                                         t_r = self.t_r, 
+                                         t_r = self.t_r,
                                          standardize=False)
         atlas_ts_conf = label_masker.fit_transform(run_img, confounds=confounds)
         self.atlas_ts_conf_ = atlas_ts_conf.copy()
-        
+
         # 3- Remove events if passed
         if events is not None:
             atlas_ts_conf_task = denoise_task(X=task_conf, Y = atlas_ts_conf)
         else:
             atlas_ts_conf_task = atlas_ts_conf.copy()
         self.atlas_ts_conf_task_ = atlas_ts_conf_task
-        
-        # 4-Standardize data 
+
+        # 4-Standardize data
         atlas_ts_clean =  standardize(atlas_ts_conf_task)
-    
+
         edge_ts = compute_edge_ts(atlas_ts_clean)
         edge_img = new_img_like(run_img, edge_ts, affine = np.eye(4)) # Add fake affine (old was:run_img.affine)
 
         return edge_img
-    
-    
+
